a5b5388fff4daa8936ba00d8bf8e585fb3b252a8
ZeroFPS-hk <57061860+ZeroFPS-hk@users.noreply.github.com>
2024-03-28 16:54:56 +0000

    Make game playable with DOM

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const gameboard = (function(){
     }
 
     const getBoard = () => board;
+    const getBoardSize = () => boardSize;
     const makeMove = (row, column, player) => {
         if(row >= boardSize || column >= boardSize) return false; //cell doesn't exist
         if(board[row][column].getValue()) return false; //cell already filled
@@ -34,15 +35,23 @@ const gameboard = (function(){
         let rightSlantDiagonal = [];
         let leftSlantDiagonal = [];
         for(i=0; i<boardSize; i++){
-            for(j=0; j<boardSize; j++){
-                rightSlantDiagonal.push(boardWithCellValues[i][j]);
-                leftSlantDiagonal.push(boardWithCellValues[i][boardSize-1-j]);
-            }
+            rightSlantDiagonal.push(boardWithCellValues[i][i]);
+            leftSlantDiagonal.push(boardWithCellValues[i][boardSize-1-i]);
         }
         if(checkLineVictory(rightSlantDiagonal)) return true;
         if(checkLineVictory(leftSlantDiagonal)) return true;
         return false; //the things I do to avoid hardcoding -.-
     };
+
+    const checkBoardFull = () => {
+        const boardWithCellValues = printBoard();
+        for (let i=0; i<boardSize; i++){
+            for(let j=0; j<boardSize; j++){
+                if(!boardWithCellValues[i][j]) return false;
+            }
+        }
+        return true;
+    }
 
     const resetBoard = () => {
         for(let row of board){
@@ -52,7 +61,7 @@ const gameboard = (function(){
         }
     };
 
-    return{getBoard, makeMove, printBoard, checkVictory, resetBoard};
+    return{getBoard, getBoardSize, makeMove, printBoard, checkVictory, checkBoardFull, resetBoard};
 })();
 
 function gamecell(){
@@ -85,4 +94,39 @@ const gameController = (function(){
     };
 
     return{getCurrentPlayer, makeMove};
+})();
+
+const displayController = (function(){
+    const boardDisplay = document.querySelector("#gameboard");
+    const messageDisplay = document.querySelector("#message");
+    const boardSize  = gameboard.getBoardSize();
+    
+    const onCellClick = (e) => {
+        let cellNumber = parseInt(e.target.id.slice(-1));
+        let row = Math.floor(cellNumber / boardSize);
+        let column = cellNumber % boardSize;
+        gameController.makeMove(row, column);
+        displayBoard();
+    }
+
+    const displayBoard = () => {
+        const board = gameboard.printBoard();
+        boardDisplay.replaceChildren();
+        for(let i=0; i<boardSize; i++){
+            let row = document.createElement("div");
+            row.classList.add("row");
+            for(let j=0; j<boardSize; j++){
+                let cell = document.createElement("div");
+                let cellNumber = i * boardSize + j;
+                cell.classList.add("cell");
+                cell.id = "cell" + cellNumber.toString();
+                cell.textContent = board[i][j];
+                cell.addEventListener("click", onCellClick);
+                row.appendChild(cell);
+            }
+            boardDisplay.appendChild(row);
+        }
+        messageDisplay.textContent = "It is " + gameController.getCurrentPlayer() + " player's turn. Click on an empty square to make your move.";
+    }
+    displayBoard();
 })();
